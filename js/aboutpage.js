92f84e385615d3a85b4c68eddae41491868d6d34
Anurag kumar <anuragkumar1252003@gmail.com>
2025-02-04 02:57:21 +0530

    //

--- js/aboutpage.js
+++ js/aboutpage.js
@@ -1,34 +1,31 @@
-let images = document.querySelectorAll('.slider img');
+let images = document.querySelectorAll(".slider img");
 let currentIndex = 0;
 
 function changeImage() {
-    images[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % images.length;
-    images[currentIndex].classList.add('active');
+  images[currentIndex].classList.remove("active");
+  currentIndex = (currentIndex + 1) % images.length;
+  images[currentIndex].classList.add("active");
 }
 setInterval(changeImage, 3000);
 
-
-
-
 const headers = ["Scan", "Location", "Path"];
 const messages = [
-    "Scan & Find Anything is an intelligent object detection tool designed to identify and locate various objects in real-time.",
-    " With just one tap, instantly find any location with speed and accuracy. No delays, no hassle—just quick and precise results at your fingertips!",
-    "Path detection helps you navigate efficiently by providing real-time guidance, ensuring you reach your destination quickly and accurately with ease."
+  "Scan & Find Anything is an intelligent object detection tool designed to identify and locate various objects in real-time.",
+  " With just one tap, instantly find any location with speed and accuracy. No delays, no hassle—just quick and precise results at your fingertips!",
+  "Path detection helps you navigate efficiently by providing real-time guidance, ensuring you reach your destination quickly and accurately with ease.",
 ];
 
 let index = 0;
 
 // Show the first message immediately when the page loads
 document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("msgHeder").textContent = headers[index];
+  document.getElementById("msg").textContent = messages[index];
+
+  // Start looping after 3 seconds
+  setInterval(() => {
+    index = (index + 1) % headers.length; // Loop through the array
     document.getElementById("msgHeder").textContent = headers[index];
     document.getElementById("msg").textContent = messages[index];
-
-    // Start looping after 3 seconds
-    setInterval(() => {
-        index = (index + 1) % headers.length; // Loop through the array
-        document.getElementById("msgHeder").textContent = headers[index];
-        document.getElementById("msg").textContent = messages[index];
-    }, 3200);
+  }, 3200);
 });
